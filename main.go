ebccdfa8f7705b21c7a2ad59a135f76d1c14ae40
jxtin <1032200960@mitwpu.edu.in>
2023-02-22 23:03:58 +0530

    small message for /

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 func main() {
 	router := gin.Default()
 
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to the CRUD API",
+		})
+	})
+
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
